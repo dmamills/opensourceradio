7917e1eabfa10262b4c3a58d36fc048fef0aa8ce
dmills <d.mills@vehikl.com>
2019-12-18 14:53:58 -0500

    Convert Header to stateless

--- client/src/Header/index.js
+++ client/src/Header/index.js
@@ -1,86 +1,66 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import cn from 'classnames';
 import { getSchedules } from '../api';
 import { p1, flex, spaceBetween, m0, whiteText, link, alignSelfCenter, mr1 } from '../styles';
 import Schedules from './Schedules';
 import UserList from './UserList';
 
-class Header extends Component {
-  state = {
-    showSchedules: false,
-    showUsers: false,
-    users: [],
-    schedules: [],
+const Header = ({ socket }) => {
+  const [showUsers, setShowUsers ] = useState(false);
+  const [showSchedules, setShowSchedules ] = useState(false);
+  const [schedules, setSchedules ] = useState([]);
+  const [users, setUsers ] = useState([]);
+
+  const onUsersChange = ({ users }) => setUsers(users);
+  const hideUsers = () =>  setShowUsers(false);
+  const hideSchedules = () => setShowSchedules(false);
+
+  const onUserlistClick = () => {
+    setShowUsers(true);
+    setShowSchedules(false);
   }
 
-  componentDidMount() {
-    const { socket } = this.props;
-    socket.on('user-joined', this.onUsersChange);
-    socket.on('user-left', this.onUsersChange);
-  }
-
-  componentWillUnmount() {
-    const { socket } = this.props;
-    socket.off('user-joined', this.onUsersChange);
-    socket.off('user-left', this.onUsersChange);
-  }
-
-  onUsersChange = ({ users }) => {
-    this.setState({
-      users
-    });
-  }
-
-  onSchedulesClick = () => {
-    const { schedules } = this.state;
-    this.setState({ showSchedules: true, showUsers: false });
-
+  const onSchedulesClick = () => {
+    setShowUsers(false);
+    setShowSchedules(true);
     if(schedules.length === 0) {
-      getSchedules()
-      .then(schedules => {
-        this.setState({ schedules });
-      });
+      getSchedules().then(schedules => setSchedules(schedules));
     }
   }
 
-  onUserlistClick = () => {
-    this.setState({ showUsers: true, showSchedules: false });
-  }
+  useEffect(() => {
+    socket.on('user-joined', onUsersChange);
+    socket.on('user-left', onUsersChange);
 
-  hideUsers = () => {
-    this.setState({ showUsers: false });
-  }
+    return function() {
+      socket.off('user-joined', onUsersChange);
+      socket.off('user-left', onUsersChange);
+    }
+  })
 
-  hideSchedules = () => {
-    this.setState({ showSchedules: false });
-  }
-
-  render() {
-    const { showSchedules, showUsers, users , schedules } = this.state;
-    return (
-      <header className={cn(flex, p1, spaceBetween)}>
-        <h1 className={cn(alignSelfCenter, m0, whiteText)}>opensourceradio</h1>
-        <div>
-          <a
-            className={cn(link, mr1)}
-            onClick={(showUsers ? this.hideUsers : this.onUserlistClick)} 
-            href="#userlist"
-          >
-            {showUsers ? 'Hide' : 'Show'} userlist
-          </a>
-          <a
-            className={link}
-            onClick={(showSchedules ? this.hideSchedules : this.onSchedulesClick)}
-            href="#schedules"
-          >
+  return (
+    <header className={cn(flex, p1, spaceBetween)}>
+      <h1 className={cn(alignSelfCenter, m0, whiteText)}>opensourceradio</h1>
+      <div>
+        <a
+          className={cn(link, mr1)}
+          onClick={(showUsers ? hideUsers : onUserlistClick)}
+          href="#userlist"
+        >
+          {showUsers ? 'Hide' : 'Show'} userlist
+        </a>
+        <a
+          className={link}
+          onClick={(showSchedules ? hideSchedules : onSchedulesClick)}
+          href="#schedules"
+        >
           {showSchedules ? 'Hide' : 'Show'} schedule
-          </a>
-        </div>
-        { showSchedules && <Schedules schedules={schedules} /> }
-        { showUsers && <UserList users={users} /> }
-      </header>
-    );
-  }
+        </a>
+      </div>
+      { showSchedules && <Schedules schedules={schedules} /> }
+      { showUsers && <UserList users={users} /> }
+    </header>
+  );
 }
 
 export default Header;
