16c7520438991254dcab27bf0717e444c4588ad1
dmills <d.mills@vehikl.com>
2019-12-18 13:39:32 -0500

    convert history to stateless, add test

--- client/src/Chat/History.js
+++ client/src/Chat/History.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useRef, useEffect } from 'react';
 import cn from 'classnames';
 import stylish from '@dmamills/stylish';
 import { p0, m0, flex, justifyEnd, column } from '../styles';
@@ -8,26 +8,25 @@ const chatBox = stylish({
   overflowY: 'scroll',
 });
 
-class History extends Component {
-  componentDidUpdate() {
-    this.messagesEl.scrollTop = this.messagesEl.scrollHeight;
-  }
+const History = ({ messages, renderMessage }) => {
+  const messagesEl = useRef(null);
 
-  render() {
-    const { messages, renderMessage } = this.props;
-    return (
-      <div 
-        className={chatBox}
-        ref={el => { this.messagesEl = el; }}
-      >
-        <ul className={cn(flex, column, justifyEnd, m0, p0)}>
-          { messages.length ? messages.map(renderMessage) :
-            renderMessage({ message: 'No Messages Yet!', name: 'Admin', timestamp: Date() })
-          }
-        </ul>
-      </div>
-    );
-  }
+  useEffect(() => {
+      messagesEl.current.scrollTop = messagesEl.current.scrollHeight;
+  });
+
+  return (
+    <div
+      className={chatBox}
+      ref={messagesEl}
+    >
+      <ul className={cn(flex, column, justifyEnd, m0, p0)}>
+        { messages.length ? messages.map(renderMessage) :
+          renderMessage({ message: 'No Messages Yet!', name: 'Admin', timestamp: Date() })
+        }
+      </ul>
+    </div>
+  );
 }
 
 export default History;
--- client/src/StreamPlayer/Volume.test.js
+++ client/src/StreamPlayer/Volume.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, waitForElement } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Volume from './Volume';
 
 describe('Volume Component', () => {
