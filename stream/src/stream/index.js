fbdce2362ede52de4dbe5a46a1f419cab20ff351
daniel mills <mills.dma@gmail.com>
2019-12-24 17:22:11 -0500

    Remove progress bar from stream process

--- stream/src/stream/index.js
+++ stream/src/stream/index.js
@@ -1,5 +1,4 @@
 const ffmpeg = require('fluent-ffmpeg');
-const progress = require('cli-progress');
 const chalk = require('chalk');
 
 const { printFfmpegHeader, getConfig } = require('../utils');
@@ -11,16 +10,6 @@ const { FFMPEG_PATH, VIDEO_PATH, STREAM_URL } = getConfig();
 if(FFMPEG_PATH && FFMPEG_PATH !== "") {
   console.log(chalk.magenta('Setting custom ffmpeg path: ', FFMPEG_PATH));
   ffmpeg.setFfmpegPath(FFMPEG_PATH);
-}
-
-const makeProgressBar = () => {
-  return new progress.Bar({ format: 'Audio Progress {bar} {percentage}% | Time Playing: {duration_formatted} |' }, progress.Presets.shades_classic);
-}
-
-const progressToSeconds = progress => {
-  const timestamp = progress.timemark.substring(0, 8);
-  const splitTimestamp = timestamp.split(':');
-  return parseInt(splitTimestamp[0], 10) * 60 * 60 + parseInt(splitTimestamp[1], 10) * 60 + parseInt(splitTimestamp[2], 10);
 }
 
 const runStream = (audioPath, metadata) => {
@@ -47,27 +36,20 @@ const runStream = (audioPath, metadata) => {
     ]);
 
     command = command.complexFilter(addFilters(metadata));
-    const progressBar = makeProgressBar();
 
     command
       .on('start', commandString => {
         printFfmpegHeader(commandString);
-        progressBar.start(Math.floor(metadata.format.duration), 0);
       })
       .on('end', () => {
-        progressBar.stop();
         resolve();
       })
       .on('error', (err, stdout, stderr) => {
-        progressBar.stop();
         reject(err);
-      })
-      .on('progress', progress => {
-        progressBar.update(progressToSeconds(progress));
       });
 
       command.save(STREAM_URL);
   });
-}
+};
 
 module.exports = runStream;
