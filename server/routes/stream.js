edd5a27d72ef5b5ade75f38290e013ad5ea0ce71
daniel mills <mills.dma@gmail.com>
2020-01-19 13:11:13 -0500

    move stream paths to env file

--- server/routes/stream.js
+++ server/routes/stream.js
@@ -1,16 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const pm2 = require('pm2');
 
 const { errorHandler, authMiddleware } = require('./middleware');
 
-//TODO: move to ENV
 const processInfo = {
-  name: 'osr_stream',
-  startScript: '/Users/mills/projects/opensourceradio/stream/index.js',
-  workingDirectory: '/Users/mills/projects/opensourceradio/stream',
+  name: process.env.STREAM_PROCESS_NAME,
+  startScript: process.env.STREAM_START_SCRIPT,
+  workingDirectory: process.env.STREAM_WORKING_DIRECTORY,
 };
-
 
 const onStatus = (res) => (err, process) => {
   if(err) return res.status(500).json({ err });
@@ -27,14 +26,14 @@ router.post('/start', authMiddleware, (req, res) => {
 });
 
 router.post('/stop', authMiddleware, (req, res) => {
-  pm2.stop('osr_stream', (err) => {
+  pm2.stop(processInfo.name, (err) => {
     if(err) res.status(500).json({ err });
     else res.json({ ok: true });
   });
 });
 
 router.get('/status', authMiddleware, (req, res) => {
-  pm2.describe('osr_stream', onStatus(res));
+  pm2.describe(processInfo.name, onStatus(res));
 });
 
 module.exports = router;
