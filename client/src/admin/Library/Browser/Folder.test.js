33bb71e76ed33b33eb17d14c1216b29de2f6077a
dmills <d.mills@vehikl.com>
2020-02-06 16:16:36 -0500

    folder tests

---
+++ client/src/admin/Library/Browser/Folder.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { fireEvent, render, act } from '@testing-library/react';
+import Folder from './Folder';
+
+describe('Folder Component', () => {
+  const folderName = 'mydir';
+  const folderContents = ['blah.mp3', 'blah2.mp3'];
+
+  it('renders without crashing', () => {
+    const { getByText } = render(<Folder folder={folderName} folderContents={folderContents} />);
+
+    expect(getByText('mydir')).toBeInTheDocument();
+  });
+
+  it('should only render the folder contents when expanded', () => {
+    const { rerender, queryByTestId, queryByText } = render(<Folder folder={folderName} folderContents={folderContents} isExpanded={true} />);
+
+    expect(queryByTestId('folder-list')).toBeInTheDocument();
+    expect(queryByText('blah.mp3')).toBeInTheDocument();
+    expect(queryByText('blah2.mp3')).toBeInTheDocument();
+
+    rerender(<Folder folder={folderName} folderContents={folderContents} isExpanded={false} />);
+    expect(queryByTestId('folder-list')).not.toBeInTheDocument();
+  });
+
+  it('should invoke the expand function when button pressed', () => {
+    const spyFn = jest.fn();
+    const { queryByTestId } = render(<Folder folder={folderName} folderContents={folderContents} actionFn={spyFn} />);
+
+    fireEvent.click(queryByTestId('folder-action'));
+    expect(spyFn).toBeCalled();
+    expect(spyFn).toBeCalledWith(folderName);
+  });
+});
